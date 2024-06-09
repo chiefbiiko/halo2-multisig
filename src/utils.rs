9f5473e1f5e46505149c780a0e64b7b88804fb5e
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2024-06-09 13:52:15 +0200

    wip: fiy impl dummy from for circuit shard

--- src/utils.rs
+++ src/utils.rs
@@ -1,17 +1,41 @@
 use axiom_codec::types::field_elements::AnySubqueryResult;
 use axiom_eth::{
     halo2_base::AssignedValue,
-    utils::component::{
-        types::{FixLenLogical, Flatten},
-        utils::get_logical_value,
-        ComponentPromiseResultsInMerkle, ComponentType, FlattenVirtualTable, LogicalResult,
+    utils::{
+        build_utils::dummy::DummyFrom,
+        component::{
+            types::{FixLenLogical, Flatten},
+            utils::get_logical_value,
+            ComponentType, FlattenVirtualTable, LogicalResult,
+        }
     },
+    providers::storage::json_to_mpt_input,
 };
 // use itertools::Itertools;
 // use serde::{Deserialize, Serialize};
 
+use axiom_query::components::subqueries::account::types::GENESIS_ADDRESS_0_ACCOUNT_PROOF;
+
 // use crate::Field;
 use axiom_query::Field;
+
+use core::marker::PhantomData;
+use axiom_query::components::subqueries::storage::types::{CircuitInputStorageShard, CircuitInputStorageSubquery};
+use ethers_core::types::EIP1186ProofResponse;
+use super::CoreParamsStorageSubquery;
+
+impl<F: Field> DummyFrom<CoreParamsStorageSubquery> for CircuitInputStorageShard<F> {
+    fn dummy_from(core_params: CoreParamsStorageSubquery) -> Self {
+        let CoreParamsStorageSubquery { capacity, max_trie_depth } = core_params;
+        let request = {
+            let pf: EIP1186ProofResponse =
+                serde_json::from_str(GENESIS_ADDRESS_0_ACCOUNT_PROOF).unwrap();
+            let proof = json_to_mpt_input(pf, 0, max_trie_depth);
+            CircuitInputStorageSubquery { block_number: 0, proof }
+        };
+        Self { requests: vec![request; capacity], _phantom: PhantomData }
+    }
+}
 
 pub(crate) fn extract_virtual_table<
     F: Field,
