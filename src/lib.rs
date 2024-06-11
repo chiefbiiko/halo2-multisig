5ed308699b9ffc862f3a4f6e18a70a696acc08fc
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2024-06-11 15:35:39 +0200

    wip: scaffolding

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,84 @@
 use halo2_base::{
-    gates::{ GateChip, GateInstructions, RangeChip, RangeInstructions },
-    halo2_proofs::halo2curves::{ bn256::Fr, secp256k1::{ Fp, Fq, Secp256k1Affine } },
-    poseidon::hasher::PoseidonHasher,
-    utils::BigPrimeField,
+    // gates::{ GateChip, GateInstructions, RangeChip, RangeInstructions },
+    // halo2_proofs::halo2curves::{ bn256::Fr, secp256k1::{ Fp, Fq, Secp256k1Affine } },
+    // poseidon::hasher::PoseidonHasher,
+    // utils::BigPrimeField,
     AssignedValue,
     Context,
-    QuantumCell,
+    // QuantumCell,
   };
-  use halo2_ecc::{
-    bigint::ProperCrtUint,
-    // bigint::{ big_is_even, ProperCrtUint },
-    // ecc::EcPoint,
-    // fields::FieldChip,
-    // secp256k1::{ hash_to_curve::{ hash_to_curve, util::fe_to_bytes_le }, Secp256k1Chip },
-  };
+  use halo2_ecc::bigint::ProperCrtUint;
+  use axiom_eth::{Field,storage::EthStorageChip, utils::component::utils::create_hasher as create_poseidon_hasher};
 
 ////////
+// /// Circuit input for a single Storage subquery.
+// #[derive(Clone, Debug, Serialize, Deserialize)]
+// pub struct CircuitInputStorageSubquery {
+//     /// The block number to access the storage state at.
+//     pub block_number: u64,
+//     /// Storage proof formatted as MPT input. It will contain the account address.
+//     /// ### Warning
+//     /// `proof.acct_pf` will be empty and `proof` will **not** have state_root set.
+//     pub proof: EthStorageInput,
+// }
+
+// #[derive(Clone, Debug, Hash, Serialize, Deserialize)]
+// pub struct EthStorageInput {
+//     pub addr: Address,
+//     pub acct_pf: MPTInput,
+//     pub acct_state: Vec<Vec<u8>>,
+//     /// A vector of (slot, value, proof) tuples
+//     pub storage_pfs: Vec<(U256, U256, MPTInput)>,
+// }
+
+/// Does **not** perform any range checks on witnesses to check if they are actually bytes.
+/// This should be done in the `parse_mpt_inclusion_phase0` function
+
+// #[derive(Clone, Debug, Hash, Serialize, Deserialize)]
+// /// The pre-assigned inputs for the MPT proof
+// pub struct MPTInput {
+//     // claim specification: (path, value)
+//     /// A Merkle-Patricia Trie is a mapping `path => value`
+//     ///
+//     /// As an example, the MPT state trie of Ethereum has
+//     /// `path = keccak256(address) => value = rlp(account)`
+//     pub path: PathBytes,
+//     pub value: Vec<u8>,
+//     pub root_hash: H256,
+//     /// Inclusion proofs will always end in a terminal node: we extract this terminal node in cases where it was originally embedded inside the last branch node.
+//     pub proof: Vec<Vec<u8>>,
+//     pub slot_is_empty: bool,
+//     pub value_max_byte_len: usize,
+//     pub max_depth: usize,
+//     pub max_key_byte_len: usize,
+//     pub key_byte_len: Option<usize>,
+// }
+////////
+
+//??? how do we go from rpc1186proofresponse to MPTInput?
+
+//WIP
+#[derive(Clone, Debug)]
+pub struct Eip1186Input<F: Field> {
+    pub safe_address: ProperCrtUint<F>,      // Safe address
+    pub msg_hash: ProperCrtUint<F>,          // Custom msg hash
+    pub state_root: ProperCrtUint<F>,        // eth_getBlockBy*::response.stateRoot
+    pub storage_root: ProperCrtUint<F>,      // eth_getProof::response.storageHash
+    pub state_trie_key: ProperCrtUint<F>,    // keccak256(safe)
+    pub storage_trie_key: ProperCrtUint<F>,  // keccak256(msg_hash + uint256(7))
+    pub account_proof: Vec<Vec<AssignedValue<F>>>, // list of bytes // eth_getProof::response.accountProof
+    pub storage_proof: Vec<Vec<AssignedValue<F>>>, // list of bytes // eth_getProof::response.storageProof.proof
+    pub header_rlp: Vec<AssignedValue<F>>, // bytes // RLP-encoded header
+}
+
+//WIP
+pub fn verify_eip1186<F: Field>(
+    ctx: &mut Context<F>,
+    chip: &EthStorageChip<F>,
+    input: Eip1186Input<F>
+) {
+
+//////// TODO refactor :: this is just the circuit beginning
 // fn virtual_assign_phase0(
 //     &mut self,
 //     builder: &mut RlcCircuitBuilder<F>,
@@ -67,32 +130,4 @@ use halo2_base::{
 // }
 ////////
 
-#[derive(Clone, Debug)]
-pub struct Eip1186Input<F: BigPrimeField> {
-pub safe_address: ProperCrtUint<F>,      // Safe address
-pub msg_hash: ProperCrtUint<F>,          // Custom msg hash
-pub state_root: ProperCrtUint<F>,        // eth_getBlockBy*::response.stateRoot
-pub storage_root: ProperCrtUint<F>,      // eth_getProof::response.storageHash
-pub state_trie_key: ProperCrtUint<F>,    // keccak256(safe)
-pub storage_trie_key: ProperCrtUint<F>,  // keccak256(msg_hash + uint256(7))
-pub account_proof: Vec<Vec<AssignedValue<F>>>, // list of bytes // eth_getProof::response.accountProof
-pub storage_proof: Vec<Vec<AssignedValue<F>>>, // list of bytes // eth_getProof::response.storageProof.proof
-pub header_rlp: Vec<AssignedValue<F>>, // bytes // RLP-encoded header
 }
-
-  pub fn verify_eip1186<F: BigPrimeField>(
-    ctx: &mut Context<F>,
-    chip: &EthStorageChip<F>,
-
-    // secp256k1_chip: &Secp256k1Chip<'_, F>,
-    // poseidon_hasher: &PoseidonHasher<F, 3, 2>,
-    // fixed_window_bits: usize,
-    // var_window_bits: usize,
-    input: Eip1186Input<F>
-  ) {
-
-
-
-
-
-  }
