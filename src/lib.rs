aa2f50c9c27b81e73cea35dbcd0ff82632c879c7
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2024-06-12 15:31:55 +0200

    wip: restruct comments

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@
 //     // QuantumCell,
 //   };
   use axiom_eth::{
-    halo2_base::{Context, gates::{RangeChip,circuit::builder::BaseCircuitBuilder}, safe_types::SafeTypeChip},
+    halo2_base::{Context, gates::{RangeChip, GateInstructions, circuit::builder::BaseCircuitBuilder}, safe_types::SafeTypeChip},
     keccak::{KeccakChip, types::ComponentTypeKeccak},
     rlp::RlpChip,
     mpt::MPTChip,
@@ -20,6 +20,8 @@
     providers::storage::json_to_mpt_input,
     storage::EthStorageChip,
     utils::{
+        hilo::HiLo,
+        circuit_utils::bytes::{unsafe_mpt_root_to_hi_lo, pack_bytes_to_hilo},
         component::{ComponentType, promise_collector::{PromiseCaller, PromiseCollector}},
         encode_addr_to_field,unsafe_bytes_to_assigned, circuit_utils::bytes::safe_bytes32_to_hi_lo, component::utils::create_hasher as create_poseidon_hasher},
         zkevm_hashes::util::eth_types::ToBigEndian,
@@ -87,6 +89,123 @@ pub fn verify_eip1186<F: Field>(
     // convert slot to HiLo to save for later
     let slot = safe_bytes32_to_hi_lo(ctx, gate, &slot_bytes);
 
+    // assign storage proof
+    let mpt_proof = mpt_proof.assign(ctx);
+    // convert storageRoot from bytes to HiLo for later. `parse_storage_proof` will constrain these witnesses to be bytes
+    let storage_root = unsafe_mpt_root_to_hi_lo(ctx, gate, &mpt_proof);
+    // Check the storage MPT proof
+    let storage_witness = chip.parse_storage_proof_phase0(ctx, slot_bytes, mpt_proof);
+    // Left pad value to 32 bytes and convert to HiLo
+    let value = {
+        let w = storage_witness.value_witness();
+        let inputs = w.field_cells.clone();
+        let len = w.field_len;
+        let var_len_bytes = SafeTypeChip::unsafe_to_var_len_bytes_vec(inputs, len, 32);
+        let fixed_bytes = var_len_bytes.left_pad_to_fixed(ctx, gate);
+        pack_bytes_to_hilo(ctx, gate, fixed_bytes.bytes())
+    };
+    // set slot value to uint256(0) when the slot does not exist in the storage trie
+    // let slot_is_empty = storage_witness.mpt_witness().slot_is_empty;
+    // let value = HiLo::from_hi_lo(value.hi_lo().map(|x| gate.mul_not(ctx, slot_is_empty, x)));
+    //FIXME assert value is 1
+   assert_eq!(value.lo().value().get_lower_32(), 1_u32);
+
+//    let block_number = ctx.load_witness(F::from(subquery.block_number));
+
+//    PayloadStorageSubquery {
+//        storage_witness,
+//        storage_root,
+//        output: AssignedStorageSubqueryResult {
+//            subquery: AssignedStorageSubquery { block_number, addr, slot },
+//            value,
+//        },
+//    }
+// }
+
+//TODO
+// let vt = extract_virtual_table(payload.iter().map(|p| p.output));
+// let lr: Vec<LogicalResult<F, Self::CompType>> =
+//     extract_logical_results(payload.iter().map(|p| p.output));
+// let ctx = pool.main();
+// // promise calls to header component:
+// let account_storage_hash_idx = ctx.load_constant(F::from(STORAGE_ROOT_INDEX as u64));
+// for p in payload.iter() {
+//     let block_number = p.output.subquery.block_number;
+//     let addr = p.output.subquery.addr;
+//     let storage_root = p.storage_root;
+//     let account_subquery =
+//         AssignedAccountSubquery { block_number, addr, field_idx: account_storage_hash_idx };
+//     let promise_storage_root = promise_caller
+//         .call::<FieldAccountSubqueryCall<F>, ComponentTypeAccountSubquery<F>>(
+//             ctx,
+//             FieldAccountSubqueryCall(account_subquery),
+//         )
+//         .unwrap();
+//     constrain_vec_equal(ctx, &storage_root.hi_lo(), &promise_storage_root.hi_lo());
+// }
+// self.payload = Some((keccak, payload));
+// CoreBuilderOutput { public_instances: vec![], virtual_table: vt, logical_results: lr }
+
+let account_storage_hash_idx = ctx.load_constant(F::from(STORAGE_ROOT_INDEX as u64));
+for p in payload.iter() {
+    let block_number = p.output.subquery.block_number;
+    let addr = p.output.subquery.addr;
+    let storage_root = p.storage_root;
+    let account_subquery =
+        AssignedAccountSubquery { block_number, addr, field_idx: account_storage_hash_idx };
+    let promise_storage_root = promise_caller
+        .call::<FieldAccountSubqueryCall<F>, ComponentTypeAccountSubquery<F>>(
+            ctx,
+            FieldAccountSubqueryCall(account_subquery),
+        )
+        .unwrap();
+    constrain_vec_equal(ctx, &storage_root.hi_lo(), &promise_storage_root.hi_lo());
+}
+
+// fn virtual_assign_phase1(&mut self, builder: &mut RlcCircuitBuilder<F>) {
+//     let (keccak, payload) = self.payload.take().unwrap();
+//     // preamble
+//     let range_chip = keccak.range();
+//     let rlc_chip = builder.rlc_chip(&range_chip.gate);
+//     let rlp = RlpChip::new(range_chip, Some(&rlc_chip));
+//     let mpt = MPTChip::new(rlp, &keccak);
+//     let chip = EthStorageChip::new(&mpt, None);
+
+//     // actual logic
+//     builder.parallelize_phase1(payload, |(ctx_gate, ctx_rlc), payload| {
+//         handle_single_storage_subquery_phase1((ctx_gate, ctx_rlc), &chip, payload)
+//     });
+
+// pub fn handle_single_storage_subquery_phase1<F: Field>(
+//    ctx: RlcContextPair<F>,
+//    chip: &EthStorageChip<F>,
+//    payload: PayloadStorageSubquery<F>,
+// ) {
+//    chip.parse_storage_proof_phase1(ctx, payload.storage_witness);
+// }
+
+    // //TODO parse_storage_proof_phase1()
+    //    // Comments below just to log what load_rlc_cache calls are done in the internal functions:
+    //     // load_rlc_cache bit_length(2*mpt_witness.key_byte_len)
+    //     chip.mpt.parse_mpt_inclusion_phase1((ctx_gate, ctx_rlc), storage_witness.mpt_witness);
+    //     // load rlc_cache bit_length(value_witness.rlp_field.len())
+    //     let value_trace =
+    //         self.rlp().decompose_rlp_field_phase1((ctx_gate, ctx_rlc), witness.value_witness);
+    //     let value_trace = value_trace.field_trace;
+    //     debug_assert_eq!(value_trace.max_len, 32);
+    //     EthStorageTrace { value_trace }
+
+
+
+
+
+
+
+
+
+
+
+//OLD
 // fn virtual_assign_phase0(
 //     &mut self,
 //     builder: &mut RlcCircuitBuilder<F>,
